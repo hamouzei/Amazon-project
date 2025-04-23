5a40b126c75ca989d78b5a340b226d8ae9c61684
hamouzie <hm164138@gmail.com>
2025-04-23 23:47:44 +0300

    make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -76,7 +76,14 @@ document.querySelector('.js-products-grid')
           quantity: 1
         });
       }
-      console.log(cart);
+
+      let cartQuantity = 0;
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+       .innerHTML = cartQuantity;
     });
   });
 
